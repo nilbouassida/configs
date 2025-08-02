5f0b3f250b687208a038edb3eb6ffc42eaaa74e3
Nil Bouassida <nil.bouassida@gmail.com>
2025-08-02 13:21:52 +0200

    nvim conf update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,90 +1,121 @@
 ---@diagnostic disable: undefined-global
-local startup_dir = vim.fn.getcwd()
+-- basic settings
+vim.o.number             = true
+vim.o.relativenumber     = true
+vim.o.signcolumn         = 'yes'
+vim.o.wrap               = false
+vim.o.tabstop            = 4
+vim.o.expandtab          = true
+vim.o.shiftwidth         = 4
+vim.opt.softtabstop      = 4
+vim.o.swapfile           = false
+vim.g.mapleader          = ' '
+vim.o.winborder          = 'rounded'
+vim.opt.ignorecase       = true
+vim.opt.incsearch        = true
+vim.opt.hlsearch         = true
+vim.opt.smartcase        = true
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = 'yes'
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.o.swapfile = false
-vim.g.mapleader = ' '
-vim.o.winborder = 'rounded'
+-- for nvim-tree
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors    = true
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :so<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>k', ':quit!<CR>')
-
-vim.keymap.set('n', '<Ctr>k', ':quit!<CR>')
+vim.keymap.set('n', '<leader>q', ':wq<CR>')
 vim.keymap.set('n', '<leader>k', ':quit!<CR>')
 
 vim.keymap.set('n', '<Esc><Esc>', ':noh<CR>')
-vim.keymap.set('v', 'K', '"+y')
+vim.keymap.set('v', 'K', '"+y<CR>')
 
--- comment out selected text
-vim.keymap.set('x', '<leader>/', function()
-    local esc = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
-    vim.api.nvim_feedkeys(esc, 'x', false)
-    local cs = vim.bo.commentstring:gsub('%%s', '')
-    vim.cmd("'<,'>normal! I" .. cs)
-end)
-
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
-    { src = 'https://github.com/vague2k/vague.nvim' },
-    { src = 'https://github.com/chomosuke/typst-preview.nvim' },
-    { src = 'https://github.com/echasnovski/mini.pick' },
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/stevearc/oil.nvim' },
-    { src = 'https://github.com/echasnovski/mini.completion' },
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = 'https://github.com/nvim-tree/nvim-tree.lua', },
+	{ src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+	{ src = 'https://github.com/junegunn/goyo.vim'},
+	{ src = 'https://github.com/jreybert/vimagit'},
 })
-
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-        vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end,
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
 })
-vim.cmd('set completeopt+=noselect')
+vim.cmd("set completeopt+=noselect")
 
-vim.lsp.enable({ 'lua_ls', 'svelte-language-server', 'tinymist', 'clangd' })
+require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+	ensure_installed = {
+		"bash",
+		"c", "cpp",
+		"css", "html",
+		"javascript", "typescript",
+		"lua",
+		"markdown", "markdown_inline",
+		"python",
+		"rust",
+		"svelte"
+	},
+	highlight = { enable = true }
+})
+require "oil".setup()
+require 'nvim-tree'.setup()
+
+-- vim.keymap.set('n', '<leader>g', ':Goyo<CR>')
+vim.keymap.set('n', '<leader>g', ':Magit<CR>')
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'tree'})
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>t', ":Oil<CR>")
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.lsp.enable({ "lua_ls", "biome", "tinymist", "emmetls" })
+
+require "vague".setup({ transparent = true })
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
 
 
 
+-- ─── Autocomplete setup ────────────────────────────────────────────────────
+-- 1. Make the built-in LSP completion autotrigger.
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client.server_capabilities.completionProvider then
+      -- For Neovim ≥0.10 use vim.lsp.completion.enable();
+      -- for 0.9.x keep using completion.enable() as below:
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      -- If you’re on 0.10 nightly, switch to:
+      -- vim.lsp.completion.enable(true, { autotrigger = true })
+    end
+  end,
+})
 
-require 'mini.pick'.setup()
-require 'oil'.setup()
+-- Recommended UI tweaks
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' } -- nicer popup
+vim.opt.shortmess:append('c')                           -- no "match x of y" echoes
 
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
-vim.keymap.set('n', '<leader>f', ':Pick files show_hidden=true<CR>')
--- vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-vim.keymap.set('n', '<leader>h', function()
-    -- Change to startup directory temporarily
-    local current_dir = vim.fn.getcwd()
-    vim.cmd('cd ' .. startup_dir)
+-- 2. Tab / Shift-Tab navigation inside the popup menu.
+local function t(str) return vim.api.nvim_replace_termcodes(str, true, true, true) end
+vim.keymap.set('i', '<Tab>', function()
+  return vim.fn.pumvisible() == 1 and t'<C-n>' or t'<Tab>'
+end, { expr = true, silent = true })
 
-    -- Get all files recursively, ignoring git
-    local files = vim.split(vim.fn.glob('**/*'), '\n')
-    files = vim.tbl_filter(function(file)
-        return vim.fn.isdirectory(file) == 0 and not file:match('%.git/')
-    end, files)
-
-    require('mini.pick').start({
-        source = { items = files, name = 'Files' }
-    })
-
-    -- Restore original directory
-    vim.cmd('cd ' .. current_dir)
-end)
+vim.keymap.set('i', '<S-Tab>', function()
+  return vim.fn.pumvisible() == 1 and t'<C-p>' or t'<S-Tab>'
+end, { expr = true, silent = true })
 
 
-vim.cmd('colorscheme vague')
-vim.cmd(':hi statusline guibg=NONE')
+
